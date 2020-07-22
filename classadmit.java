ed06a0818fc12c59e45063eb0cb8571375856689
SOURAV <56021891+hattymatty1@users.noreply.github.com>
2020-07-22 20:43:40 +0530

    Create classadmit.java

---
+++ classadmit.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class classadmit {
+    public static void main(String[] args) {
+    
+        String name;
+        int age;
+		int i;
+        int j;
+        int n;
+        int[] marks = new int[5];
+    
+        Scanner inp = new Scanner(System.in);
+        n = inp.nextInt();
+
+        for(i = 1; i <= n; i++);
+        {
+            public void setInfo() {
+                System.out.println("Enter detail:");
+                name = Sc.nextline();
+                age = Sc.nextline();
+                for(j = 0; j < 5; j++)
+                marks[i] = Sc.nextln();
+            }    
+                
+            public void getInfo() {
+                System.out.println("name = "+name);
+                System.out.println("age = "+age);
+                System.out.println("marks = "+marks);
+                for(j = 0; j < 5;i++)
+                {
+                    System.out.println(marks[j]);
+                }
+
+            }
+                
+            
+        }
+    }
+
+
+}
